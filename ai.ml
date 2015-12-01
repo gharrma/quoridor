373207b85d4e9e088eed044ba0d6932a9a506254
Matthew Gharrity <mjg355@cornell.edu>
2015-12-01 18:38:28 -0500

    AI stubs added; implementation soon

--- ai.ml
+++ ai.ml
@@ -1,5 +1,16 @@
 include Model
 
+(* Returns the number of moves required to win for a given player. *)
+let dist_to_win game player_id =
+  failwith "TODO"
+
+(* Returns a list of all possible moves that a given player can make. *)
+let get_valid_moves game player_id = 
+  failwith "TODO"
+
+let minimax game player_id =
+  failwith "TODO"
+
 let next_move game player_id =
   let pos = game.players.(player_id) in
   Move pos
