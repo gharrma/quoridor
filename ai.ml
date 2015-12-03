cd52b2b2635da8e86f743a9d1f28772d80216396
victoreis <vor3@cornell.edu>
2015-12-02 23:08:05 -0500

    added path cutting

--- ai.ml
+++ ai.ml
@@ -2,7 +2,7 @@ open Model
 
 (* Returns the number of moves required to win for a given player. *)
 let dist_to_win board player_id =
-  let (locn, _) = (board.players).(player_id) in let (iy, ix) = locn in
+  let ((iy, ix), _) = (board.players).(player_id) in
   let q = Queue.create() in
   let n = board.size in
   let dist = Array.init n (fun x -> Array.init n (fun x -> -1)) in
@@ -20,7 +20,7 @@ let dist_to_win board player_id =
 	  in List.iter (chk_neighbor (py, px))
     [(py-1, px); (py+1, px); (py, px-1); (py, px+1)]
   done;
-  let dto y x = if (dist.(y).(x) > 0) then dist.(y).(x) else max_int in
+  let dto y x = if (dist.(y).(x) >= 0) then dist.(y).(x) else max_int in
   let rec ans a b = if (b = 0) then dto a 0 else min (dto a b) (ans a (b-1)) in
 	if player_id = 0 then ans (n-1) (n-1) else ans 0 (n-1)
 
@@ -58,32 +58,96 @@ let get_valid_moves board player_id =
   (fun m -> validate_move player_id m board)
   (moves @ wall_placements)
 
+ (* Returns list consisting of a possible path to victory (no jumping),
+  where the head is the current position of the player
+  and the last element is a winning position. *)
+let rec path_to_win game player_id =
+  let (py, px) = fst game.players.(player_id) in
+  if(dist_to_win game player_id = 0) then [(py, px)] else let n = game.size in
+  let mlist = List.filter (fun (dy, dx) ->
+                           let (my, mx) = (py + dy/2, px + dx/2) in
+                           mx >= 0 && my >= 0 && mx <= 2*n-2 && my <= 2*n-2 &&
+                           not (game.board.(my).(mx) = Wall))
+                          [(0,2); (0,-2); (2,0); (-2,0)] in
+  let rec closest = function
+    |[] -> (Move(py, px), max_int)
+    |(dy, dx)::ptl -> let m = Move(py+dy, px+dx) in
+                      (commit_move player_id m game);
+                      let d = dist_to_win game player_id in
+                      (undo player_id m game (py, px));
+                      let (pm, dis) = closest ptl in if (d < dis) then (m, d)
+                      else (pm, dis)
+  in let pm = fst (closest mlist) in (commit_move player_id pm game);
+  let path = path_to_win game player_id in (undo player_id pm game (py, px));
+  (py, px)::path
+
+let rec printpath = function
+  | [] -> ()
+  | [(y, x)] -> Printf.printf "(%d, %d)\n%!" x y
+  | (y, x)::tl -> (Printf.printf "(%d, %d)--" x y); printpath tl
+
+let pathtoarray path n =
+  let ans = Array.init (2*n-1) (fun y -> Array.init (2*n-1) (fun x -> false)) in
+  let rec fill = function
+    |[] -> ()
+    |[(y, x)] -> (ans.(y).(x) <- true);
+    |(ay, ax)::(by, bx)::c -> (ans.(ay).(ax) <- true); (ans.(by).(bx) <- true);
+                              (ans.((ay+by)/2).((ax+bx)/2) <- true);
+                              fill ((by,bx)::c)
+  in (fill path); ans
+
+let printparray patharray =
+  Array.iter(fun y ->
+    Array.iter(fun x -> Printf.printf "%d " (if x then 1 else 0)
+    ) y; print_newline()
+  ) patharray
+
+(* Checks whether the move consists of a wall cutting the given path. *)
+let cutspath move patharray =
+  match move with
+  |Move(y, x) -> false
+  |PlaceWall wlist -> let rec checkwalls = function
+                      |[] -> false
+                      |(wy, wx)::wtl -> patharray.(wy).(wx) || checkwalls wtl
+                    in checkwalls wlist
+
 (* Returns a list of movements that achieve the maximal value of minimal value
 the opponent can force the player into, where the value of a position is defined
-as the difference between the distances of the players to their respective edge
- of the board *)
+as ???????? *)
+
+let ismove = function
+  |Move(x, y) -> true
+  |_ -> false
+
 let minimax game player_id =
   let op_id = 1 - player_id in
   let pml = get_valid_moves game player_id in
   if List.length pml = 0 then Printf.printf "Oopsy daisy!\n%!";
-  Printf.printf "now pscore = %d, enemy = %d\n%!"
-            (dist_to_win game op_id) (dist_to_win game player_id);
+  let (odist, pdist) = (dist_to_win game op_id, dist_to_win game player_id) in
+  (Printf.printf "now pdist = %d, odist = %d\n%!" pdist odist);
+  let ppath = pathtoarray (path_to_win game player_id) game.size in
+  let opath = pathtoarray (path_to_win game op_id) game.size in
   let (ploc, oloc) = (fst game.players.(player_id), fst game.players.(op_id)) in
-	let rec best game oid pid = function
+  let counter = ref 0 in
+	let rec best game = function
 	  |[] -> (-max_int, [])
-	  |pm::ptl -> (commit_move pid pm game);
-				  let oml = get_valid_moves game oid in
+	  |pm::ptl -> (commit_move player_id pm game);
+				  let oml = get_valid_moves game op_id in
 				  let rec worse b = function
 					|[] -> max_int
-					|om::otl -> (commit_move oid om b);
-            let (x, y) = (dist_to_win b pid, dist_to_win b oid) in
+					|om::otl -> (commit_move op_id om b);
+            let x = if (cutspath om ppath) || (cutspath pm ppath) || ismove pm
+                    then ((incr counter); dist_to_win b player_id) else pdist in
+            let y = if (cutspath om opath) || (cutspath pm opath) || ismove om
+                    then ((incr counter); dist_to_win b op_id) else odist in
             let a = if (y <= 8) then y*(16-y) else 4*y + 32 in
-            let d = a - 8*x in (undo oid om b oloc); min d (worse b otl)
-				  in let w = worse game oml in (undo pid pm game ploc);
-				  let (prevbest, bestmoves) = best game oid pid ptl in
+            let d = a - 8*x in (undo op_id om b oloc); min d (worse b otl)
+				  in let w = worse game oml in (undo player_id pm game ploc);
+				  let (prevbest, bestmoves) = best game ptl in
 				  if (w = prevbest) then (w, pm::bestmoves) else
 				  if (w > prevbest) then (w, [pm]) else (prevbest, bestmoves)
-  in snd (best game op_id player_id pml)
+  in let ans = snd (best game pml) in
+  (Printf.printf "Called dist_to_win %d times!\n%!" !counter); ans
 
 let next_move game player_id =
   let (ploc, nwalls) = game.players.(player_id) in
