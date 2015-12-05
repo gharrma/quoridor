a0794d7eb0ce5cb1ac0c7d26590c9fd1118bbe1a
Matthew Gharrity <mjg355@cornell.edu>
2015-12-04 22:38:00 -0500

    Stylize model.ml

--- model.ml
+++ model.ml
@@ -27,6 +27,7 @@ type move =
   | Move of loc
   | PlaceWall of wall
 
+(* Prints an ASCI version of the game board. *)
 let print_game t =
   Array.iter (fun row ->
     Array.iter (function
@@ -50,115 +51,130 @@ let create_board size =
     else NoWall in
   let fill_row y = Array.init rep_size (fill_space y) in
   let board = Array.init rep_size fill_row in
-
   { size; board; players }
 
 (* Returns whether a location is unacessible, either because it has a wall,
- is not a valid board location or is occupied by a player. *)
+   is not a valid board location or is occupied by a player. *)
 let haswall board y x =
   let n = Array.length board.board in
-  if(y < 0 || y >= n || x < 0 || x >= n) then true else
-  match (board.board).(y).(x) with
-  |Wall -> true
-  |Player i -> true
-  |_ -> false
+  if y < 0 || y >= n || x < 0 || x >= n then true
+  else
+    match (board.board).(y).(x) with
+    | Wall -> true
+    | Player _ -> true
+    | _ -> false
 
 let commit_move player_id move board =
   let ((py, px), nwalls) = (board.players).(player_id) in
   match move with
   | Move(y, x) ->
-    (board.board.(py).(px) <- Space);
-    (board.board.(y).(x) <- Player player_id);
-    (board.players.(player_id) <- ((y, x), nwalls))
-
+    board.board.(py).(px) <- Space;
+    board.board.(y).(x) <- Player player_id;
+    board.players.(player_id) <- ((y, x), nwalls)
   | PlaceWall wlist ->
     let rec updatewalls = function
       | [] -> ()
       | (y, x)::tl ->
         (board.board.(y).(x) <- Wall); updatewalls tl
-    in (updatewalls wlist);
+    in 
+    updatewalls wlist;
     (board.players.(player_id) <- ((py, px), nwalls - 1))
-
 
 let undo player_id move board prevloc =
   let (_, nwalls) = (board.players).(player_id) in
   let (py, px) = prevloc in
   match move with
-  |Move(y, x) ->
-    (board.board.(py).(px) <- Player player_id);
-    (board.board.(y).(x) <- Space);
-    (board.players.(player_id) <- ((py, px), nwalls))
-
-  |PlaceWall wlist ->
+  | Move(y, x) ->
+    board.board.(py).(px) <- Player player_id;
+    board.board.(y).(x) <- Space;
+    board.players.(player_id) <- ((py, px), nwalls)
+  | PlaceWall wlist ->
     let rec destroy = function
     | [] -> ()
     | (y, x)::tl ->
       (board.board.(y).(x) <- NoWall); destroy tl
-    in (destroy wlist);
+    in 
+    destroy wlist;
     (board.players.(player_id) <- ((py, px), nwalls + 1))
 
 
 (* Returns the point in the plane corresponding to the reflection of a over b *)
 let reflect a b =
-  let (ya, xa) = a in let (yb, xb) = b in (2*yb - ya, 2*xb - xa)
+  let (ya, xa) = a in
+  let (yb, xb) = b in
+  (2*yb - ya, 2*xb - xa)
 
 let validate_move player_id move board =
   let ((py, px), nwalls) = (board.players).(player_id) in
   match move with
-  | Move(y, x) -> if haswall board y x then false else begin
-    if(abs(px - x) + abs(py - y) = 2) then
+  | Move(y, x) -> 
+    if haswall board y x then 
+      false
+    else if abs(px - x) + abs(py - y) = 2 then
       not (haswall board ((py + y)/2) ((px + x)/2))
-    else if(abs(px - x) + abs(py - y) > 4) then false (* else dist = 4 *)
-    else let (my, mx) = ((py + y)/2, (px + x)/2) in
-    if (px = x || py = y) then (* jumping *)
-      haswall board my mx &&
-      not (haswall board ((my + py)/2) ((mx + px)/2)) &&
-      not (haswall board ((my + y)/2) ((mx + x)/2))
-    else (* diagonal move *)
-      let can1 =
-        not (haswall board py mx) &&
-        not (haswall board my x) &&
-        ((let (wy, wx) = reflect (py, mx) (py, x) in haswall board wy wx)
-        ||
-        (let (qy, qx) = reflect (py, px) (py, x) in haswall board qy qx))
-      in
-      let can2 =
-        not (haswall board my px) &&
-        not (haswall board y mx) &&
-        ((let (wy, wx) = reflect (my, px) (y, px) in haswall board wy wx)
-        ||
-        (let (qy, qx) = reflect (py, px) (y, px) in haswall board qy qx))
-      in
-      match (board.board.(py).(x), board.board.(y).(px)) with
-        |(Player a, Player b) -> can1 || can2
-        |(Player a, _) -> can1
-        |(_, Player b) -> can2
-        |_ -> false
-    end
+    else if abs(px - x) + abs(py - y) > 4 then
+      false
+    else (* dist = 4 *)
+      let (my, mx) = ((py + y)/2, (px + x)/2) in
+      if (px = x || py = y) then (* jumping *)
+        haswall board my mx &&
+        not (haswall board ((my + py)/2) ((mx + px)/2)) &&
+        not (haswall board ((my + y)/2) ((mx + x)/2))
+      else (* diagonal move *)
+        let can1 =
+          let (wy, wx) = reflect (py, mx) (py, x) in
+          let (qy, qx) = reflect (py, px) (py, x) in
+          not (haswall board py mx) &&
+          not (haswall board my x) &&
+          (haswall board wy wx || haswall board qy qx)
+        in
+        let can2 =
+          let (wy, wx) = reflect (my, px) (y, px) in
+          let (qy, qx) = reflect (py, px) (y, px) in
+          not (haswall board my px) &&
+          not (haswall board y mx) &&
+          (haswall board wy wx || haswall board qy qx)
+        in
+        begin match board.board.(py).(x), board.board.(y).(px) with
+          | Player a, Player b -> can1 || can2
+          | Player a, _        -> can1
+          | _       , Player b -> can2
+          | _                  -> false
+        end
   | PlaceWall wlist -> begin
     if (nwalls = 0) then false else
     let rec canplace = function
-    |[] -> true
-    |(y, x)::tl -> not (haswall board y x) && canplace tl in
-    if (not (canplace wlist)) then false else begin
-    (commit_move player_id move board);
-    let n = board.size in
-    let mark = Array.init n (fun x -> Array.init n (fun x -> false)) in
-    let found = ref false in
-    let dirs = [|(-2, 0);(2, 0);(0, -2);(0, 2)|] in
-    let rec visit mark found player_id (y, x) =
-      if (x < 0 || y < 0 || x >= 2*n || y >= 2*n || mark.(y/2).(x/2))
-        then ()
-      else begin mark.(y/2).(x/2) <- true;
-      if (player_id = 0 && y = 2*n - 2) then found := true else
-      if (player_id = 1 && y = 0) then found := true else
-      for i = 0 to 3 do let (dy, dx) = dirs.(i) in
-        if(not (haswall board (y + dy/2) (x + dx/2)))
-        then visit mark found player_id (y + dy, x + dx) else () done end
-    in (visit mark found 0 (fst board.players.(0)));
-    let ans0 = !found in (found := false);
-    (Array.iter (fun row -> Array.fill row 0 n false) mark);
-    (visit mark found 1 (fst board.players.(1)));
-    (undo player_id move board (py, px));
-    ans0 && !found
-    end end
+      |[] -> true
+      |(y, x)::tl -> not (haswall board y x) && canplace tl
+    in
+    if not (canplace wlist) then false
+    else begin
+      (commit_move player_id move board);
+      let n = board.size in
+      let mark = Array.init n (fun x -> Array.init n (fun x -> false)) in
+      let found = ref false in
+      let dirs = [|(-2, 0);(2, 0);(0, -2);(0, 2)|] in
+      let rec visit mark found player_id (y, x) =
+        if x < 0 || y < 0 || x >= 2*n || y >= 2*n || mark.(y/2).(x/2)
+          then ()
+        else begin 
+          mark.(y/2).(x/2) <- true;
+          if player_id = 0 && y = 2*n - 2 then found := true else
+          if player_id = 1 && y = 0 then found := true else
+          for i = 0 to 3 do 
+            let (dy, dx) = dirs.(i) in
+            if not (haswall board (y + dy/2) (x + dx/2))
+              then visit mark found player_id (y + dy, x + dx)
+              else ()
+          done
+        end
+      in 
+      visit mark found 0 (fst board.players.(0));
+      let ans0 = !found in
+      found := false;
+      Array.iter (fun row -> Array.fill row 0 n false) mark;
+      visit mark found 1 (fst board.players.(1));
+      undo player_id move board (py, px);
+      ans0 && !found
+    end
+    end
